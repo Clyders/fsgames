843296dc1d5250c95579a1a9e7a02d03b06abafd
Aditya <105863819+Clyders@users.noreply.github.com>
2025-04-17 16:29:28 +0530

    test

--- src/functions/games/gtn/startGuessNumber.ts
+++ src/functions/games/gtn/startGuessNumber.ts
@@ -14,13 +14,11 @@ export default new NativeFunction({
   version: "1.0.0",
   description: "Starts a 'Guess the Number' game.",
   unwrap: false,
-  args: [
-    Arg.requiredString("options", "Game configuration options."),
-    Arg.optionalString(
-      "result",
-      "Env variable to store results (Default: result)",
-    ),
-  ],
+args: [
+  Arg.optionalString("options", "Game configuration options."),
+  Arg.optionalString("result", "Env variable to store results (Default: result)"),
+],
+
   brackets: true,
   async execute(ctx) {
     let env = this.data.fields![1];
